ac1106e6a4ff692c7225df59b7f1102a3a3a6f17
sheldon <xiadanli0320@gmail.com>
2020-02-12 17:06:28 +0800

    polls index and detail

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from .models import Question
 
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Hello World, It's Polls")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    context = {
+        'question': question
+    }
+
+    return render(request, 'polls/detail.html', context)
+
+
+def results(request, question_id):
+    return HttpResponse("result of question {}".format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("voting on question {}".format(question_id))
